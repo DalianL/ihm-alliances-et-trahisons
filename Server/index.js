2edb86c29a637a08f70920ea15ca769994035d6d
Loic Dalian <dl100463@etu.unice.fr>
2018-01-14 21:58:42 +0100

    Push de la première version du serveur

---
+++ Server/index.js
@@ -0,0 +1,26 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+var userId = 0;
+io.on('connection', function(socket) {
+  socket.userId = userId ++;
+  console.log('a user connected, user id: ' + socket.userId);
+
+  socket.on('hello', function(msg){
+    console.log('message from user#' + socket.userId + ": " + msg);
+    io.emit('hello', {
+      id: socket.userId,
+      msg: msg
+    });
+  });
+
+});
+
+http.listen(8000, function(){
+  console.log('listening on *:8000');
+});
