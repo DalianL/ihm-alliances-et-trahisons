c393daf9567d9ddc13c1ef29c3b5dc40b92de290
Axel Aiello <axel.aiello.interactive4d@gmail.com>
2018-01-19 09:15:35 +0100

    Without Big File

---
+++ ClientMobile/Assets/Demo/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AssemblyCSharp
+{
+	[Serializable]
+	public class Player
+	{
+		public int id;
+		public string pseudo;
+		public string color;
+
+		public Player ()
+		{
+			new Player (0,"", "");
+		}
+
+		public Player (int id, string pseudo, string color)
+		{
+			this.id = id;
+			this.pseudo = pseudo;
+			this.color = color;
+		}
+	}
+}
+
