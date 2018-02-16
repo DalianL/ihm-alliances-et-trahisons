29a806e28135d8657858dddc710b0b3572f08847
matt10456 <emeta01@yahoo.fr>
2018-02-16 09:00:02 +0100

    Visualisation du titre pour toutes les orientations

--- AT/src/menu.js
+++ AT/src/menu.js
@@ -46,6 +46,9 @@ export default function buildMenu() {
 
   $('#example-container').append('<h1 style="padding-top:250px; color:white;"> Alliances et Trahisons</h1>');
   $('#example-container').append('<div align="center" style="margin:150px;"><canvas id="canvas"></canvas></div>');
+  $('#example-container').append('<h1 class="reverseTitle" style="color:white;"> Alliances et Trahisons</h1>');
+  $('#example-container').append('<h1 class="leftTitle" style="color:white;"> Alliances et Trahisons</h1>');
+  $('#example-container').append('<h1 class="rightTitle" style="color:white;"> Alliances et Trahisons</h1>');
   QRCode.toCanvas(document.getElementById('canvas'), 'localhost');
 
   $('#example-container').append('<button id="user-test" class="menu-button"> Lancer le jeu </button></br>');
